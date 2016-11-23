3f28606b38eef79659b71ff87748f4de62baba71
atlanthot <n.legrand28@gmail.com>
2016-11-23 14:04:15 +0100

    + ajout des classes Wizzard et MagicWand

--- javascript/javascript/exo0/js/main_prototype.js
+++ javascript/javascript/exo0/js/main_prototype.js
@@ -45,8 +45,95 @@ Warrior.prototype.isDead		= function()
 };
 
 
-var mere_theresa = new Warrior("Mère Theresa");
+
+function MagicWand(){}
+
+MagicWand.prototype.consoMana 	= 40;
+MagicWand.prototype.degats 		= 20;
+
+
+
+function HarryPotterStick(){}
+
+HarryPotterStick.prototype.consoMana 	= 100;
+HarryPotterStick.prototype.degats 		= 35;
+
+
+
+function Wizzard(param_nom)
+{
+	this.nom = param_nom;
+}
+
+Wizzard.prototype.nom 			= "Merlin";
+Wizzard.prototype.mana 			= 100;
+Wizzard.prototype._pv 			= 100;
+Wizzard.prototype._degats 		= 3;
+Wizzard.prototype._wandstick 	= null; 
+
+Wizzard.prototype.setMagicWand	= function(param_magicwand)
+{
+	this._wandstick = param_magicwand;
+};
+
+Wizzard.prototype.getMagicWand	= function()
+{
+	return this._wandstick;
+};
+
+Wizzard.prototype.attack		= function(param_ennemy)
+{
+	var booster = Math.round( Math.random() * 10 );
+	
+	if( this._wandstick == null )
+	{
+		param_ennemy.getDammages(this._degats + booster);
+	}
+	else if( this.mana <= this._wandstick.consoMana )
+	{
+		param_ennemy.getDammages(this._degats + booster);
+	}
+	else
+	{
+		param_ennemy.getDammages(this._wandstick.degats + booster);
+	}
+};
+
+Wizzard.prototype.getDammages 	= function(param_degats)
+{
+	this._pv -= param_degats;
+	
+	if( this._pv < 0 )
+	{
+		this._pv = 0;
+	}
+};
+
+Wizzard.prototype.getPV			= function()
+{
+	return this._pv;
+};
+
+Wizzard.prototype.isDead		= function()
+{
+	if( this._pv <= 0 )
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+};
+
+
+
+
+
+var mere_theresa = new Wizzard("Mère Theresa");
 var robocop = new Warrior("Robocop");
+
+mere_theresa.setMagicWand( new HarryPotterStick() );
 
 
 while( !mere_theresa.isDead() && !robocop.isDead() )
