425a89458ef8c8c236a39d725d973bacf4764f55
atlanthot <n.legrand28@gmail.com>
2016-11-23 15:50:11 +0100

    + introduction à la programmation évenementielle

--- javascript/javascript/exo0/js/main.js
+++ javascript/javascript/exo0/js/main.js
@@ -1,29 +1,23 @@
 // ES5
 "use strict";
 
-/*
-function start()
+
+
+function google(param_event)
 {
-	var element = document.getElementById("test");
-	console.log(element);
+	alert("youpi !");
+}
+
+function start(param_event)
+{
+	var mon_bouton = document.getElementById("super_bouton");
+	
+	mon_bouton.addEventListener("click", google);
 	
 	window.removeEventListener("load", start);
 }
 
-
 window.addEventListener("load", start);
-
-
-*/
-
-
-// javascript va créer un nouvel objet vide : même chose que faire new Object();
-
-// puis Javascript va analyser la propriété prototype de la fonction utilisée pour créer l'objet: en l'occurrence, la fonction Warrior. 
-
-// sur notre objet vide, crée à l'étape 1, Javascript va créer autant de nouvelles propriétés, qu'il y en a sur le prototype de la fonction, et il va leur attribuer les valeurs par défaut, définies sur le prototype.
-
-
 
 
 
