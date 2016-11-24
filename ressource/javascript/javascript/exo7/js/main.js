9401a40734f91bfe4f8935b549c70fa066d97aef
ezamLinux <ezam.linux@openmailbox.org>
2016-11-24 16:07:19 +0100

    rangement

---
+++ ressource/javascript/javascript/exo7/js/main.js
@@ -0,0 +1,38 @@
+function main()
+{
+	var video = document.getElementById("uncharted");
+	var btn = document.getElementById("btn");
+	btn.addEventListener("click", snapshot);
+	
+	video.loop = true;
+	video.muted = true;
+	video.autoplay = false;
+	video.poster = "poney.jpg";
+}
+
+function snapshot()
+{
+	btn.removeEventListener("click", snapshot);
+	
+	var canvas = document.getElementById("projector");
+	var context = canvas.getContext("2d");
+	var video = document.getElementById("uncharted");
+	
+	
+	canvas.width = video.width;
+	canvas.height = video.height;
+	
+	context.clearRect(0,0,canvas.width, canvas.height);
+	
+	
+	context.save();
+	context.drawImage(video,0,0,video.width, video.height);
+	context.restore();
+	
+	setTimeout( snapshot, 5 );
+}
+
+
+
+
+window.addEventListener("load", main);
