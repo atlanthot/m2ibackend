20a4760e87647bef72e12acb22a5967d9d1efa91
atlanthot <n.legrand28@gmail.com>
2016-12-01 15:03:19 +0100

    + add A* implementation

--- ressource/algo/implementation_grenouilles.js
+++ ressource/algo/implementation_grenouilles.js
@@ -1,11 +1,13 @@
 function Grenouille(){}
 
-function Marecage()
+function Marecage(param_distance)
 {
-	this.grenouilles = new Array();
+	this.distance 		= param_distance
+	this.grenouilles 	= new Array();
 }
 
 Marecage.prototype.grenouilles = null;
+Marecage.prototype.distance = 0;
 
 
 
@@ -15,47 +17,51 @@ var grenouillere = [
 					new Grenouille(),
 					new Grenouille(),
 					new Grenouille(),
+					new Grenouille(),
 					new Grenouille()
 				];
 				
 var marecages = [
-			new Marecage(),
-			new Marecage(),
-			new Marecage(),
-			new Marecage(),
-			new Marecage(),
-			new Marecage(),
-			new Marecage(),
-			new Marecage(),
-			new Marecage(),
-			new Marecage()
+			new Marecage(10),
+			new Marecage(2),
+			new Marecage(20)
 ];
 
 var max = grenouillere.length;
 var i 	= 0;
 var j	= 0;
 var o	= 0;
-var z 	= 0;
+var z 	= max;
+var d	= 0;
+
 var currentGrenouille = null;
+var currentMarecage = null;
 
 
 while( grenouillere.length > 0 )
 {
 	o = 0;
-	z = grenouillere.length;
+	z = max;
 	j = marecages.length;
+	d = 10000;
+	
+	currentGrenouille = grenouillere.shift();
+	
 	
 	while( --j > -1 )
 	{
-		if( marecages[j].grenouilles.length <= z )
+		currentMarecage = marecages[j];
+		
+		if( currentMarecage.grenouilles.length <= z && currentMarecage.distance <= d )
 		{
-			z = marecages[j].grenouilles.length;
+			z = currentMarecage.grenouilles.length;
+			d = currentMarecage.distance;
 			o = j;
 		}
 	}
 	
 	
-	currentGrenouille = grenouillere.shift();
+	
 	marecages[o].grenouilles.push(currentGrenouille);
 }
 
