cae946e7f3dbacd40ad3dce924d7cf5100001223
atlanthot <n.legrand28@gmail.com>
2016-12-09 16:07:37 +0100

    + ajout des premiers codes php

--- back_end/lesson_1/index.php
+++ back_end/lesson_1/index.php
@@ -1,56 +1,45 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link type="text/css" href="main.css" rel="stylesheet" />
+</head>
+<body>
+	<div class="map">
+		<?php
+			$map = array(
+				array(0,0,0,0,0,0),
+				array(0,0,0,0,0,0),
+				array(0,0,0,1,0,0),
+				array(0,1,0,0,0,0),
+				array(0,0,0,0,1,0),
+				array(0,0,0,0,0,0)
+			);
+
+			foreach( $map as $row )
+			{
+				foreach( $row as $col )
+				{
+					if ($col == 0)
+					{
+						echo '<span class="red"></span>';
+					}
+					else
+					{
+						echo '<span class="green"></span>';
+					}
+			}
+				
+				
+				echo '<br/>';
+			}
+		?>
+	</div>
+</body>
+</html>
 
 
-
-$toto = "johann";
-$$toto = 800; 		// == $johann = 800;
-
-
-//echo($johann);
-
-
-
-$toto = true; 
-$toto = false;
-
-
-
-
-// tableaux non typés, commes les variables, on peut mettre tout ce que l'on veut à l'intérieur
-$toto = array(30,20,800,"johann","rudy", array());
-
-
-$toto[0]; // = 30;
-
-// tableaux en mode dictionnaire de données sous forme de paire clé / valeur
-$toto = array(
-	"rudy" => 100,
-	"melissa" => 200,
-	"farid"=> 300
-);
-
-
-//echo $toto["rudy"]; // affiche 100
-
-/*
-
-// boucle for normale 
-
-$i 		= 0;
-$max 	= count($toto);
-
-for( $i = 0; $i < $max; $i++ )
-{
-	echo $toto[$i];
-}
-
-*/
-
-
-foreach( $toto as $key=>$value )
-{
-	echo $key.":".$value."<br/>";
-}
 
 
 
