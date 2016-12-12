dce87fe6e48a451476ede5837b56e8985ea3b8eb
atlanthot <n.legrand28@gmail.com>
2016-12-12 12:15:19 +0100

    + ajout du code sur l'héritage + surcharge + portée des opérateurs (protected public private)

--- back_end/lesson_2/lesson_2.php
+++ back_end/lesson_2/lesson_2.php
@@ -18,9 +18,12 @@ var_dump($voiture);
 
 class Voiture
 {
-	public $price 		= 1;
-	protected $_marque 	= null;
-	private $_conso 	= 1;
+	public $price 		= 1; // tout le monde peut utiliser cette propriété
+	
+	protected $_marque 	= null; //  les classes filles pourront utiliser cette propriété mais pas les élements à l'extérieur de l'objet
+	
+	private $_conso 	= 1; // les classes filles ne pourront pas utiliser cette propriété
+	
 	
 	// la fonction constructrice, peut prendre soit le nom __construct
 	// soit le nom de la classe. Elle sera appelée, à chaque nouvelle
@@ -28,7 +31,6 @@ class Voiture
 	
 	// dès qu'un paramètre possède une valeur par défaut, tout ceux qui le suivent
 	// doivent également en avoir une.
-	
 	public function Voiture($param_price 	= 1, 
 							$param_marque 	= null, 
 							$param_conso 	= 1
@@ -40,7 +42,7 @@ class Voiture
 		
 		$this->setConso($param_conso);
 		
-		echo("nouvel objet de type voiture !");
+		echo("nouvel objet de type voiture ! <br/> <br/>");
 	}
 	
 	
@@ -60,13 +62,89 @@ class Voiture
 		return $this->_conso;
 	}
 	
+	protected function _startEngine()
+	{
+		echo 'allume le moteur <br/>';
+	}
 	
+	protected function _startLights()
+	{
+		echo 'allume les phares <br/>';
+	}
+	
+	protected function _stopEngine()
+	{
+		echo 'allume le moteur <br/>';
+	}
+	
+	protected function _stopLights()
+	{
+		echo 'allume les phares <br/>';
+	}
+	
+	
+	public function start()
+	{
+		echo "*********** demarre la voiture ************* <br/>";
+		$this->_startEngine();
+		$this->_startLights();
+		echo "******************************************** <br/>";
+	}
+	
+	public function stop()
+	{
+		echo "*********** stoppe la voiture ************* <br/>";
+		$this->_stopEngine();
+		$this->_stopLights();
+		echo "******************************************** <br/>";
+	}
 	
 	
 }
 
-$voiture_de_johann = new Voiture();
 
-var_dump($voiture_de_johann);
+class Batmobile extends Voiture
+{
+	
+	public function Batmobile(
+							$param_price 	= 1, 
+							$param_marque 	= null, 
+							$param_conso 	= 1
+	)
+	{
+		parent::Voiture($param_price, $param_marque, $param_conso);
+		
+		echo '<h1>en fait je suis une batmobile ! </h1><br/>';
+	}
+	
+	
+	protected function _startBatRockets()
+	{
+		echo 'allume les batrockets ! <br/>';
+	}
+	
+	// surcharge de la fonction parente
+	public function start()
+	{
+		/*
+		parent::start();
+		$this->_startBatRockets();
+		*/
+		
+		echo "*********** demarre la batmobile ************* <br/>";
+		$this->_startEngine();
+		$this->_startLights();
+		$this->_startBatRockets();
+		echo "******************************************** <br/>";
+	}
+	
+}
+
+$batmobile = new Batmobile();
+
+$batmobile->start();
+$batmobile->stop();
+
+
 
 
