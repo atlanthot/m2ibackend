57cc3bfc249ce70316be5aca186c1845f9b93b9c
Jeanmy <jeanmickaelcoppin@gmail.com>
2016-12-12 11:24:21 +0100

    exo2.php

--- back_end/Jeanmy/php/exo2.php
+++ back_end/Jeanmy/php/exo2.php
@@ -18,3 +18,20 @@ class Voiture
 $voiture_de_johann = new Voiture();
 
 var_dump($voiture_de_johann);
+
+
+// la fonction constructrice peut prendre soit le nom__construct
+//soit le nom de la classe . elle sera appelée, à chque nouvelle
+//création d'objet
+
+public function Voiture(
+						$param_price = 0,
+						$param_marque = null,
+						$param_conso = 0
+						)
+
+{
+	$this->price = $param_price;
+	$this->marque = $param_marque;
+	$this->conso = $param_conso;
+}
