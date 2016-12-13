edb8987a4ef7b4b37aeb41e1d8147d28e9c7fd0d
atlanthot <n.legrand28@gmail.com>
2016-12-13 11:48:52 +0100

    + add multiton exercice

---
+++ back_end/lesson_2/multiton.php
@@ -0,0 +1,11 @@
+/*
+ * 
+ * de la même manière que vous pouvez obtenir une seule et unique instance d'une même classe en faisant appel à la méthode statique getInstance, lors de l'exercice précédent, vous devez pouvoir créer une seule et unique  instance par clé. 
+ * 
+ * 
+ * 
+ * 
+ * Exemple: 
+	Personnage::getInstance("Merlin") me renverra toujours la même instance de Personnage (le même objet de type personnage) MAIS Personnage::getInstance("Arthur") me renverra un objet différent de Personnage::getInstance("Merlin").
+ * 
+ * */
