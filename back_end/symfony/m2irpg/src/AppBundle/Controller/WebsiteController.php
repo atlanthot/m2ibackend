c72b16ed59fb093ba14cfb2e9f67f6694399b122
atlanthot <n.legrand28@gmail.com>
2017-01-05 11:13:53 +0100

    + exercice -> modifier la gallerie:
    - les images doivent être contenues à l'intérieur d'un dossier interne au site web ( on n'utilise plus de lien en http://wwww.site_externe.com/super_image.jpg par exemple )
    - on crée une feuille de style interne au site
    - Puis on fait le lien à l'intérieur des templates vers ces élements nouvellement crées à l'aide de la fonction asset() de symfony qui disponible au sein des templates Twig.
    
    Nota Bene: Courage à toi jojo !

--- back_end/symfony/m2irpg/src/AppBundle/Controller/WebsiteController.php
+++ back_end/symfony/m2irpg/src/AppBundle/Controller/WebsiteController.php
@@ -11,25 +11,51 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route as Route;
 
 class WebsiteController extends Controller
 {
+
+
+	private function _getMenu()
+	{
+		$home_url 		= $this->generateUrl('home_route'			, array()	);
+		$contact_url 	= $this->generateUrl('contact_route'		, array()	);
+		$products_url 	= $this->generateUrl('products_route'		, array()	);
+		$forum_url 		= $this->generateUrl('forum_route'			, array()	);
+		$classroom_url 	= $this->generateUrl('classroomById_route'	, array('student_id'=>5)	);
+		$galery_url 	= $this->generateUrl('galery_route'			, array()	);
+		
+		
+		$menu = array( 	
+						'home'=>$home_url,
+						'contact'=>$contact_url,
+						'products'=>$products_url,
+						'forum'=>$forum_url,
+						'classroom'=>$classroom_url,
+						'galery'=>$galery_url
+					);
+					
+		return $menu;
+	}
     
     public function home()
     {
         return $this->render('home.html.twig', array(
-			'section_title'=>'home'
+			'section_title' => 'home',
+			'menu'=>$this->_getMenu()
 		));
     }
     
     public function contact()
     {
 		return $this->render('contact.html.twig', array(
-			'section_title'=>'contact'
+			'section_title'=>'contact',
+			'menu'=>$this->_getMenu()
 		));
     }
     
     public function products()
     {
 		return $this->render('products.html.twig', array(
-			'section_title'=>'products'
+			'section_title'=>'products',
+			'menu'=>$this->_getMenu()
 		));
     }
 	
@@ -38,16 +64,14 @@ class WebsiteController extends Controller
     {
 		return $this->render('forum.html.twig', array(
 			'section_title' => 'forum',
-			'post_id'=>$post_id
+			'post_id'=>$post_id,
+			'menu'=>$this->_getMenu()
 		));
     }
     
-    public function classroom($student_id)
+    public function classroomById($student_id)
     {
-		return $this->render('classroom.html.twig', array(
-			'section_title' => 'classroom',
-			'student_id'	=>$student_id,
-			'students_list' => array(
+		$list =  array(
 			
 				'Christophe',
 				'Frédéric',
@@ -63,8 +87,62 @@ class WebsiteController extends Controller
 				'Farid',
 				'Melissa'
 				
-			)
+		);
+	
+		if ( $student_id >= count($list) )
+		{
+			$student_id = count($list) - 1;
+		}
+		
+		if ( $student_id < 0 )
+		{
+			$student_id = 0;
+		}
+	
+		return $this->render('classroom.html.twig', array(
+			'section_title' 	=>'classroom',
+			'selected_student'	=>$list[$student_id],
+			'students_list' 	=>$list,
+			'menu'				=>$this->_getMenu()
 		));
+    } 
+	 
+    public function classroomByName($student_name)
+    {
+		$list =  array(
+			
+				'Christophe',
+				'Frédéric',
+				'Johann',
+				'Mohammed',
+				'Nicolas',
+				'Richard',
+				'Thomas',
+				'Adberrahmane',
+				'Alexandre',
+				'Rudy',
+				'Younes',
+				'Farid',
+				'Melissa'
+				
+		);
+		
+		if ( !in_array($student_name, $list) )
+		{
+			$student_name = 'no_student';
+		}
+		
+	
+		return $this->render(
+			'classroom.html.twig', 
+			array
+			(
+				'section_title' => 'classroom',
+				'selected_student'	=>$student_name,
+				'students_list' => $list,
+				'menu'=>$this->_getMenu()
+			)
+		);
     } 
 	
 	public function galery($img_id)
@@ -76,13 +154,15 @@ class WebsiteController extends Controller
 			'http://img.lum.dolimg.com/v1/images/starwars_551c43f4.jpeg'
 		);
 		
-		return $this->render('galery.html.twig', 
-								array(
-									'section_title'=>'galery',
-									'img_list' => $images,
-									'img_id'=>$img_id
-								)
-							);
+		return $this->render(
+						'galery.html.twig', 
+						array(
+							'section_title'=>'galery',
+							'img_list' => $images,
+							'img_id'=>$img_id,
+							'menu'=>$this->_getMenu()
+						)
+		);
 	}
 	
 	
