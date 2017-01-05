512d5b9c8d532f0dfe21367aa48b86bbf8b5d556
atlanthot <n.legrand28@gmail.com>
2017-01-05 15:30:16 +0100

    + ajout des fichiers yml et de l'entité Student

--- back_end/symfony/m2irpg/src/AppBundle/Controller/WebsiteController.php
+++ back_end/symfony/m2irpg/src/AppBundle/Controller/WebsiteController.php
@@ -148,10 +148,10 @@ class WebsiteController extends Controller
 	public function galery($img_id)
 	{
 		$images = array(
-			'http://fr.web.img2.acsta.net/c_215_290/pictures/15/04/09/12/10/228112.jpg',
-			'https://mi-od-live-s.legocdn.com/r/www/r/starwars/-/media/franchises/starwars2015/misc/tfa_cta_744x421.jpg?l.r2=-521045527',
-			'https://media.starwars.ea.com/content/starwars-ea-com/fr_FR/starwars/battlefront/news-articles/collect-iconic-heroes-and-dominate-the-universe-in-star-wars-gal/_jcr_content/featuredImage/renditions/rendition1.img.jpg',
-			'http://img.lum.dolimg.com/v1/images/starwars_551c43f4.jpeg'
+			'img/image_1.jpg',
+			'img/image_2.jpg',
+			'img/image_3.jpg',
+			'img/image_4.jpg'
 		);
 		
 		return $this->render(
