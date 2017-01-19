e69e9c5d2b3463692603e48b920a8e98f66eefa7
nicolaspatte <nicolas.patte@live.fr>
2017-01-19 14:32:43 +0100

    Ajout du dossier et des fichiers du projet

---
+++ Projet/m2i/src/AppBundle/Controller/WebServiceController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller as Controller;
+use Symfony\Component\HttpFoundation\Response as Response;
+use Symfony\Component\HttpFoundation\JsonResponse as JsonResponse;
+
+class WebServiceController extends Controller
+{
+   
+    public function presentation()
+	{
+		$articles = $this	->getDoctrine()
+							->getRepository('AppBundle:Articles')
+							->findAll();
+							
+		$tabArticles = array();
+		foreach($articles as $item) 
+		{
+			$tabArticles[] = array	(
+										'id' => $item->getId(),
+										'id_user' => $item->getId_user(),
+										'title' => $item->getTitle(),
+										'content' => $item->getContent(),
+										'category' => $item->getCategory(),
+										'creation_date' => $item->getCreationDate(),
+										'modification_date' => $item->getModificationDate(),
+									);
+		}
+		return new JsonResponse($tabArticles);
+	}
+}
